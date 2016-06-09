bb878ff9f86a8283bc30e53e3490c8be211fc97e
jjun14 <jimmy.s.jun@gmail.com>
2016-06-09 13:56:57 -0700

    Initial Commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app')
+    }
+    // // CSS
+    // { 
+    //   test: /\.styl$/, 
+    //   include: path.join(__dirname, 'app'),
+    //   loader: 'style-loader!css-loader!stylus-loader'
+    // }
+    ]
+  }
+};
